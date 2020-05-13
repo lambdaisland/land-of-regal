033a3d220f890d8d4f3fc4af2ffe1be6a05c47a3
Arne Brasseur <arne@arnebrasseur.net>
2020-05-13 17:08:22 +0200

    First release!

---
+++ docs/ui/main.js
@@ -0,0 +1,3 @@
+(function(){
+if("undefined"!==typeof Symbol){var a=Symbol;"object"!=typeof a||!a||a instanceof Array||a instanceof Object||Object.prototype.toString.call(a)};console.log("123");
+}).call(this);
